81359ec30b30d6052dd18afaab13393839b31112
rosiekg123 <rosiekg123@aol.com>
2023-02-05 15:36:40 +0000

    added js to add time data

--- script/app.js
+++ script/app.js
@@ -23,3 +23,37 @@ let parisTimeformatted = moment()
 
 let citySelect = document.querySelector("#cities");
 citySelect.addEventListener("change", showSelectedCity);
+
+function updateTime() {
+  //London Time
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment();
+  londonDateElement.innerHTML = londonTime.format("dddd, MMMM Do, YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+
+  //Sydney Time
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = sydneyTime.format("dddd, MMMM Do, YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+
+  //New York Time
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorkTime.format("dddd, MMMM Do, YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
